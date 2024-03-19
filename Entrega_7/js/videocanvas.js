33bfca14a4921be8b751de35eef36a623796b898
DavidDell <delorza001@ikasle.ehu.eus>
2024-03-19 13:43:15 +0100

    Apartados d y e, falta el b

--- Entrega_7/js/videocanvas.js
+++ Entrega_7/js/videocanvas.js
@@ -1,29 +1,38 @@
 var efecto = null;
-var clip = "videos/demovideo1"; // nombre del vídeo, sin extensión
+var clip = "videos/demovideo1"; 
+var video;
 
 window.onload = function() {
+    video = document.getElementById("video");
 
-    var video = document.getElementById("video");
     var botonByN = document.getElementById("byn");
     botonByN.onclick = cambiarEfecto;
+
     var botonNormal = document.getElementById("normal");
     botonNormal.onclick = cambiarEfecto;
+
     var pausaButton = document.getElementById("pausa");
     pausaButton.onclick = togglePausa;
+
     var cienciaFiccionButton = document.getElementById("cienciaFiccion");
     cienciaFiccionButton.onclick = cambiarEfecto;
+
+    var pipButton = document.getElementById("pip");
+    pipButton.onclick = togglePIP;
+
+    var playAudioButton = document.getElementById("playAudio");
+    playAudioButton.onclick = reproducirAudio;
 
     video.addEventListener("play", procesarFrame, false);
 
     video.src = clip + getFormatExtension();
     video.load();
     video.play();
-
 }
 
-function cambiarEfecto(e){
+function cambiarEfecto(e) {
     var id = e.target.getAttribute("id");
-    if (id == "byn" ){
+    if (id == "byn") {
         efecto = byn;
     } else if (id == "cienciaFiccion") {
         if (efecto === scifi) {
@@ -37,21 +46,16 @@ function cambiarEfecto(e){
 }
 
 function getFormatExtension() {
-    var video = document.getElementById("video");
     if (video.canPlayType("video/mp4") != "") {
         return ".mp4";
-    }
-    else if (video.canPlayType("video/ogg") != "") {
+    } else if (video.canPlayType("video/ogg") != "") {
         return ".ogv";
-    }
-    else if (video.canPlayType("video/webm") != "") {
+    } else if (video.canPlayType("video/webm") != "") {
         return ".webm";
     }
 }
 
 function procesarFrame(e) {
-    var video = document.getElementById("video");
-
     if (video.paused || video.ended) {
         return;
     }
@@ -69,21 +73,19 @@ function procesarFrame(e) {
         var r = frame.data[i * 4 + 0];
         var g = frame.data[i * 4 + 1];
         var b = frame.data[i * 4 + 2];
-        if (efecto){
+        if (efecto) {
             efecto(i, r, g, b, frame.data);
         }
     }
+
+
     display.putImageData(frame, 0, 0);
 
     setTimeout(procesarFrame, 0);
-    // en los navegadores modernos, es mejor usar :
-    // requestAnimationFrame(procesarFrame);
-
 }
 
 function byn(pos, r, g, b, data) {
-    var gris = (r+g+b)/3;
-
+    var gris = (r + g + b) / 3;
     data[pos * 4 + 0] = gris;
     data[pos * 4 + 1] = gris;
     data[pos * 4 + 2] = gris;
@@ -92,15 +94,45 @@ function byn(pos, r, g, b, data) {
 function scifi(pos, r, g, b, data) {
     var offset = pos * 4;
     data[offset] = Math.round(255 - r);
-    data[offset+1] = Math.round(255 - g) ;
-    data[offset+2] = Math.round(255 - b) ;
+    data[offset + 1] = Math.round(255 - g);
+    data[offset + 2] = Math.round(255 - b);
 }
 
 function togglePausa() {
-    var video = document.getElementById("video");
     if (video.paused) {
         video.play();
     } else {
         video.pause();
+    }
+}
+
+function reproducirAudio() {
+    loadAudio("audio/soundtrack.mp3").then(audio => audio.play());
+}
+
+function loadAudio(src) {
+    return new Promise((resolve, reject) => {
+        var audio = new Audio();
+        audio.src = src;
+        audio.addEventListener('canplay', function() {
+            resolve(audio);
+        });
+        audio.addEventListener('error', function() {
+            reject('Error cargando el audio');
+        });
+    });
+}
+
+function togglePIP() {
+    if (video !== document.pictureInPictureElement) {
+        video.requestPictureInPicture()
+            .catch(error => {
+                console.error('Error al entrar en modo Picture in Picture:', error);
+            });
+    } else {
+        document.exitPictureInPicture()
+            .catch(error => {
+                console.error('Error al salir del modo Picture in Picture:', error);
+            });
     }
 }
