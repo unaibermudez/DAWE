ba186519110563230d39d76d50486c029d07283c
DavidDell <delorza001@ikasle.ehu.eus>
2024-04-04 10:34:26 +0200

    Ordenado el codigo y actualizado gitignore

--- Entrega_9/server.js
+++ Entrega_9/server.js
@@ -4,13 +4,10 @@ const multer = require("multer");
 
 const app = express();
 
-// Define the path to the public folder
 const publicPath = path.join(__dirname, "public");
 
-// Serve static files from the public folder
 app.use(express.static(publicPath));
 
-// Multer configuration
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, path.join(__dirname, "public/imgs"));
@@ -21,29 +18,25 @@ const storage = multer.diskStorage({
 });
 
 const fileFilter = function (req, file, cb) {
-    // Accept only jpg and png files
     if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
         cb(null, true);
     } else {
-        // Error if file format is not correct
         cb(new Error('Formato de archivo no válido. Solo se permiten archivos jpg y png.'), false);
     }
 };
 
 const upload = multer({ 
     storage: storage,
-    limits: { fileSize: 2 * 1024 * 1024 }, // 2MB limit
+    limits: { fileSize: 2 * 1024 * 1024 }, 
     fileFilter: fileFilter
 });
 
-// Handle file uploads
 app.post("/upload/files", upload.single('file'), (req, res) => {
     // Check if file was uploaded
     if (!req.file) {
         return res.status(400).json({ success: false, message: "No file uploaded" });
     }
 
-    // Prepare response data
     const responseData = {
         success: true,
         file: {
@@ -53,11 +46,9 @@ app.post("/upload/files", upload.single('file'), (req, res) => {
         }
     };
 
-    // Send response
     res.status(200).json(responseData);
 });
 
-// Start the server
 const port = 3000;
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
