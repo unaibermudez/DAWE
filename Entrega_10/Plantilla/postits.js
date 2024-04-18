e8bf8a79670cdd6b0fed696f33c28703b7fd471a
Unai Bermudez <unaberosa@gmail.com>
2024-04-18 14:48:25 +0200

    añadir plantilla entrega 10

---
+++ Entrega_10/Plantilla/postits.js
@@ -0,0 +1,45 @@
+/* postits.js
+ *
+ */
+
+window.onload = init;
+
+function init() {
+	var button = document.getElementById("add_button");
+	button.onclick = createSticky;
+
+	// cargar las notas postit de localStorage  
+	// cada nota se guarda como un par así: postit_X = texto_de_la_nota
+	// donde X es el número de la nota
+	// por cada una de ellas, llamar al método
+	// addStickyToDOM(texto_de_la_nota);
+}
+
+function createSticky() {
+	var value = document.getElementById("note_text").value;
+	
+        // crear la nota con nombre postit_X, donde X es un número entero
+	// (postit_1, postit_2, ...)  y guardarla en el localStorage
+	
+	addStickyToDOM(value);
+}
+
+
+function addStickyToDOM(value) {
+	var stickies = document.getElementById("stickies");
+	var postit = document.createElement("li");
+	var span = document.createElement("span");
+	span.setAttribute("class", "postit");
+	span.innerHTML = value;
+	postit.appendChild(span);
+	stickies.appendChild(postit);
+}
+
+function clearStickyNotes() {
+	// Crear un nuevo botón en la ventana de postit notes que al pulsarlo,
+	// elimine las notas de pantalla y de localStorage
+	// Algoritmo:	
+	// obtener una referencia a la capa "stickies"
+	// recorrer los hijos (childNodes) de esa referencia,
+	// eliminándolos uno a uno (removeChild)
+}
