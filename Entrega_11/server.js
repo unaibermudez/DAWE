7c86d74c2c37651bde86995a20c3c35ec77adfc5
Unai Bermudez <unaberosa@gmail.com>
2024-04-28 20:12:57 +0200

    intentar una cosa pero no va

--- Entrega_11/server.js
+++ Entrega_11/server.js
@@ -11,19 +11,37 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Middleware para verificar la autenticación del usuario
 function requireAuth(req, res, next) {
     const authToken = req.get('Authorization');
+    console.log('Authorization:', authToken);
 
     // Verificar si el usuario está autenticado
     if (!authToken) {
-        return res.status(401).send('Please login first');
+        res.sendFile(path.join(__dirname, 'public', 'please-login.html')); // Archivo que muestra "Please log in"
+    } else {
+        // Continuar con el siguiente middleware si el usuario está autenticado
+        next();
     }
-    // Continuar con el siguiente middleware si el usuario está autenticado
-    next();
 }
 
 // Ruta para servir la página de usuarios protegida
 app.get('/users', requireAuth, (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'users.html'));
+    // Obtener datos del usuario autenticado (si existen)
+    const currentUser = firebase.auth().currentUser;
+    console.log('currentUser:', currentUser); 
+    let userData = null;
+
+    if (currentUser) {
+        userData = {
+            displayName: currentUser.displayName,
+            email: currentUser.email,
+            photoURL: currentUser.photoURL,
+            uid: currentUser.uid
+        };
+    }
+
+    // Renderizar la página de usuarios con los datos del usuario (o null si no está autenticado)
+    res.render('users', { user: userData });
 });
+
 
 // Ruta para servir la página de inicio
 app.get('/', (req, res) => {
